b9eb278b24c9142fde0d60cca4fa3a3f13bae312
pwneal <pneal@umass.edu>
2016-02-27 04:21:13 -0500

    finish brain activity detection with blink lapse time

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ var client = neurosky.createClient({
 
 	var starttime = Date.now()/1000;
 	var blinkcapture =[];
-	var blinkcount = 0;
+	var blink_lapse = 0;
+
+	var lastblinked = Date.now()/1000;;
+
+console.log("wait 5 seconds, then blink 5 times to initialize stats");
 
 // bind receive data event
 client.on('data',function(data){
@@ -18,29 +22,46 @@ client.on('data',function(data){
 		wss.broadcast(data);
 	}
 
+	//The average person blinks some 15-20 times per minute, or every 3 second 
+	//if detect blinking get the time lapse of blinks
 	if(data.blinkStrength !=null){
-		console.log(data);
-		blinkcount =blinkcount +1;
+		//console.log(data);
+		lastblinked = Date.now()/1000;
 
 		var curtime = Date.now()/1000;
-		var elapsetime = (curtime- starttime);
-		blinkcapture.push({'elapsetime':elapsetime.toFixed(1)});
-		console.log(blinkcapture.slice(1).slice(-5));
+		var elapsetime = (curtime- starttime).toFixed(1);
+		blinkcapture.push({'elapsetime':elapsetime});
+		//console.log(blinkcapture.slice(1).slice(-5));   // print last 5 second 
 
 		var len = blinkcapture.length
 		if (len>5){
-			var difference = parseFloat(blinkcapture[len-1]['elapsetime'])-parseFloat(blinkcapture[len-5]['elapsetime']);
-			console.log("5sec blink lapse time " + difference.toFixed(1));
-
-
-			blinkcapture = blinkcapture.slice(1).slice(-6);
+			blink_lapse = ((parseFloat(blinkcapture[len-1]['elapsetime'])-parseFloat(blinkcapture[len-5]['elapsetime']))/5).toFixed(1);
+			console.log("avg sec/blink: " + blink_lapse);
+			blinkcapture = blinkcapture.slice(-5); // cut out old data
 		}
 
 
 	}
 	
+	//if not blink a long time and detecting high meditation, then you might be sleeping
 	if (data.eSense != null){
 		//console.log(data.eSense);
+		//console.log(data.eSense['meditation']);
+
+		//console.log(lastblinked);
+
+		var curtime = Date.now()/1000;
+		var timelapse_blinked = (curtime- lastblinked).toFixed(1);
+		//console.log(timelapse_blinked);
+		var brain_activity = 100 - parseFloat(data.eSense['meditation']);
+		console.log(brain_activity);
+
+		if (brain_activity<10 && ( blink_lapse > 5 || timelapse_blinked>30) ){
+			console.log("wake uppppppppp!!!!!" );
+			console.log("| last blinked: " + timelapse_blinked + 
+						"| average blink lapse: "  + blink_lapse +
+						"| brain activity: " + brain_activity);
+		}
 	}
 
 	if (data.eegPower != null){
